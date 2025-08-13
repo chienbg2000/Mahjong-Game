ffb930550272cdaae9d0b7215883629d5e489624
chienbg2000 <57701993+chienbg2000@users.noreply.github.com>
2025-08-14 00:56:57 +0700

    scale theo tỉ lệ màn

--- src/js/index.js
+++ src/js/index.js
@@ -18,6 +18,12 @@ $(function(){
     const pai   = Majiang.UI.pai($('#loaddata'));
     const audio = Majiang.UI.audio($('#loaddata'));
 
+    function applyBoardTransformForViewport() {
+        const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+        const scaleRatio = viewportWidth && viewportWidth < 800 ? viewportWidth / 800 : 1;
+        document.documentElement.style.setProperty('--board-scale', String(scaleRatio));
+    }
+
     const analyzer = (kaiju)=>{
         $('body').addClass('analyzer');
         return new Majiang.UI.Analyzer($('#board > .analyzer'), kaiju, pai,
@@ -27,6 +33,7 @@ $(function(){
         $('#board .controller').addClass('paipu')
         $('body').attr('class','board');
         scale($('#board'), $('#space'));
+        applyBoardTransformForViewport();
         const _viewer
                 = new Majiang.UI.Paipu(
                         $('#board'), paipu, pai, audio, 'Majiang.pref',
@@ -45,21 +52,6 @@ $(function(){
     let rule = Majiang.rule(
                     JSON.parse(localStorage.getItem('Majiang.rule')||'{}'));
 
-    function applyBoardTransformScale() {
-        const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-        const $boardRoot = $('#board');
-        if (viewportWidth && viewportWidth < 800) {
-            const s = viewportWidth / 800;
-            $boardRoot.css('transform-origin', '0% 0%')
-                      .css('transform', `scale(${s})`);
-            $('#board > .board').css('transform', 'none');
-            $('#board > .controller').css('transform', 'none');
-        }
-        else {
-            $boardRoot.css('transform', '');
-        }
-    }
-
     function start() {
         let players = [ new Majiang.UI.Player($('#board'), pai, audio) ];
         for (let i = 1; i < 4; i++) {
@@ -72,7 +64,7 @@ $(function(){
         $('#board .controller').removeClass('paipu')
         $('body').attr('class','board');
         scale($('#board'), $('#space'));
-        applyBoardTransformScale();
+        applyBoardTransformForViewport();
 
         new Majiang.UI.GameCtl($('#board'), 'Majiang.pref', game, game._view);
         game.kaiju();
@@ -86,7 +78,7 @@ $(function(){
 
     $('#file .start').on('click', start);
 
-    $(window).on('resize', ()=>scale($('#board'), $('#space')));
+    $(window).on('resize', ()=>{ scale($('#board'), $('#space')); applyBoardTransformForViewport() });
 
     setTimeout(()=>{
         $(window).on('load', function(){
@@ -97,21 +89,21 @@ $(function(){
             $presets.find('.preset[data-preset="default"]').on('click', ()=>{
                 clearSelector('title');
                 rule = Majiang.rule({});
-                applyBoardTransformScale();
+                applyBoardTransformForViewport();
                 start();
             });
             // Mリーグルール
             $presets.find('.preset[data-preset="Mリーグルール"]').on('click', ()=>{
                 clearSelector('title');
                 rule = Majiang.rule(require('./conf/rule.json')['Mリーグルール']);
-                applyBoardTransformScale();
+                applyBoardTransformForViewport();
                 start();
             });
             // Classicルール
             $presets.find('.preset[data-preset="Classicルール"]').on('click', ()=>{
                 clearSelector('title');
                 rule = Majiang.rule(require('./conf/rule.json')['Classicルール']);
-                applyBoardTransformScale();
+                applyBoardTransformForViewport();
                 start();
             });
             show($presets);
